959aaacae5a3cfc59d082342bcc2fac73b39e86e
ttngocthao <ttngocthao_87@yahoo.com>
2021-01-08 15:49:18 +0000

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: [
+        {
+            resolve: `gatsby-plugin-netlify-cms`,
+            options: {
+                enableIdentityWidget: true,
+            },
+        },
+    ]
+}

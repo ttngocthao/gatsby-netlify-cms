c5a95ed6b47496642807513fcfa670d2fbb428c2
ttngocthao <ttngocthao_87@yahoo.com>
2021-01-08 16:37:08 +0000

    added packgage for markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,19 @@
 module.exports = {
     plugins: [
         {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+            path: `${__dirname}/src/pages`,
+            name: "pagesData",
+        },
+        },
+        { resolve: `gatsby-remark-source-name` },
+        {
             resolve: `gatsby-plugin-netlify-cms`,
             options: {
                 enableIdentityWidget: true,
             },
         },
+        `gatsby-transformer-remark`,
     ]
 }

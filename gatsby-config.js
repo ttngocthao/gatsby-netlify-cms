574773760ea738fb17f63e220aec595a751ad21a
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2021-01-09 23:52:31 +0000

    added markdown field to product page in config.yml

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/data/pages/homePage`,
+        path: `${__dirname}/src/data/pages/home`,
         name: "homePageData",
       },
     },

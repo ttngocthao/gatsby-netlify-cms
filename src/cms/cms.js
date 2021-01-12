1976c1662ece0280368d5123034535f53e005eb5
ttngocthao <ttngocthao_87@yahoo.com>
2021-01-12 15:25:05 +0000

    added cms utils and admin css file

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import CMS from 'netlify-cms-app'
+import './cms-utils'
 
 
 import ProductPagePreview from '../components/Product/ProductPage/ProductPage'
-CMS.registerPreviewStyle('/styles.css')
+
 CMS.registerPreviewTemplate('products',({entry})=><ProductPagePreview {...entry.toJS().data} />)

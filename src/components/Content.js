09c5ee75ef6e3ffb77bdf9f1844c0bbfd9f3df77
ttngocthao <ttngocthao_87@yahoo.com>
2021-01-13 12:14:29 +0000

    added contact page

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Marked from 'react-markdown'
+
 
 
 export const HTMLContent =({content,className=''})=>{
---
+++ src/pages/contact.js
@@ -0,0 +1,71 @@
+import React,{useState} from 'react'
+
+const Contact = () => {
+    const [inputVals,setInputVals]=useState({
+        name:'',
+        email:'',
+        role:'leader',
+        message:''
+    })
+    const {name,email,role,message} = inputVals
+    const contactSubmitHandle =async(e)=>{
+        e.preventDefault()
+        console.log(inputVals)
+        if(name==='' || email==='' || role==='' || message===''){
+            alert('Input boxes cannot be empty')
+            return
+        }
+        try {
+            const res = await fetch('/',{
+                method:'POST',
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                body: new URLSearchParams(inputVals).toString()
+            })
+            console.log('Form successfully submitted',res)
+        } catch (error) {
+            alert('error')
+            console.log(error)
+        }
+        
+
+    }
+    const inputChangeHandle = (e)=>{
+        setInputVals({
+            ...inputVals,
+            [e.target.name]: e.target.value
+        })
+    }
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <h2>This page's purpose is to use netlify forms to handle contact form for the site.</h2>
+            <form onSubmit={contactSubmitHandle}
+                name='contact-form' 
+                data-netlify={true} 
+                data-netlify-honeypot="bot-field"
+                data-netlify-recaptcha="true">
+                <p>
+                    <label>Your Name: <input value={name} type="text" name="name" onChange={inputChangeHandle}/></label>   
+                </p>
+                <p>
+                    <label>Your Email: <input value={email} type="email" name="email" onChange={inputChangeHandle}/></label>
+                </p>
+                <p>
+                    <label>Your Role: 
+                        <select name="role" onChange={inputChangeHandle} value={role}>
+                    <option value="leader">Leader</option>
+                    <option value="follower">Follower</option>
+                    </select></label>
+                </p>
+                <p>
+                    <label>Message: <textarea value={message} name="message" onChange={inputChangeHandle}></textarea></label>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
+            </form>
+        </div>
+    )
+}
+
+export default Contact

31dd662e41c730fc13ab4303fd821f5ab2a97434
ttngocthao <ttngocthao_87@yahoo.com>
2021-01-14 15:31:34 +0000

    added fields for coffee page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,13 @@ import { graphql, Link } from "gatsby";
 const Index = ({ data }) => {
   // const products = data.allMarkdownRemark.nodes[0].frontmatter.products;
 
-  const productsData = data.allMarkdownRemark.nodes.filter(
-    (item) => item.fields.sourceName === "productsData"
-  );
-  const homePageData = data.allMarkdownRemark.nodes.filter(
-    (item) => item.fields.sourceName === "homePageData"
-  )[0].frontmatter;
-  console.log("products", productsData);
-  console.log("homePageData", homePageData);
+  // const productsData = data.allMarkdownRemark.nodes.filter(
+  //   (item) => item.fields.sourceName === "productsData"
+  // );
+  // const homePageData = data.allMarkdownRemark.nodes.filter(
+  //   (item) => item.fields.sourceName === "homePageData"
+  // )[0].frontmatter;
+ 
   return (
     <div>
       <h1>{homePageData.pageTitle}</h1>
